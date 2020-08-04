c0a6e3dce4a587034ccc0add0cb76904cef40973
Toxe <toxe@toxe.net>
2020-08-04 15:07:14 +0200

    Add navigation

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 import Pages from "./Pages";
+import Navigation from "./Navigation";
 
 export default function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="pages/*" element={<Pages />} />
-        </Routes>
+        <div>
+            <Navigation />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="about" element={<About />} />
+                <Route path="pages/*" element={<Pages />} />
+            </Routes>
+        </div>
     );
 }
