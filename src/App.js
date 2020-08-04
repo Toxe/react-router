114122eba44910f97e8ce140c40d13afc8028ab0
Toxe <toxe@toxe.net>
2020-08-04 14:27:23 +0200

    Home and About pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
 
 export default function App() {
-    return <div>App</div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
