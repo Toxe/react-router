6ea2bde226e0cf69c87f52b1e02a9bbe2c82ff04
Toxe <toxe@toxe.net>
2020-08-04 21:44:15 +0200

    Style navigation

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,20 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Nav = styled.div`
+    display: flex;
+    padding: 10px;
+    background: #d3d3ff;
+`;
+
+const Item = styled.div`
+    > a {
+        font-weight: bold;
+    }
+
+    &:not(:last-child)::after {
+        content: "|";
+        padding: 0px 10px 0px 10px;
+    }
+`;
 
 export default function Navigation() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/pages">Pages</Link>
-                </li>
-            </ul>
-        </nav>
+        <Nav>
+            <Item><Link to="/">Home</Link></Item>
+            <Item><Link to="/about">About</Link></Item>
+            <Item><Link to="/pages">Pages</Link></Item>
+        </Nav>
     );
 }
