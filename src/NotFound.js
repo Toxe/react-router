ea6cfa58736f1b49baafdfc77d2d9aa903af348a
Toxe <toxe@toxe.net>
2020-08-05 12:33:07 +0200

    Add styling to not found message

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,5 +1,14 @@
 import React from "react";
+import styled from "styled-components";
+
+const Message = styled.div`
+    background: red;
+    color: white;
+    font-weight: bold;
+    margin-top: 20px;
+    padding: 10px;
+`;
 
 export default function NotFound() {
-    return <div>Resource not found.</div>;
+    return <Message>Resource not found.</Message>;
 }
