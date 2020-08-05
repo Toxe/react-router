60e648a2424fb2d713e16fa6c0adf03bb6708bb7
Toxe <toxe@toxe.net>
2020-08-05 12:27:50 +0200

    Add styling to home and about page

--- src/About.js
+++ src/About.js
@@ -1,10 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+
+const Content = styled.div`
+    background: #b6fdc7;
+    padding: 10px;
+`;
+
+const Title = styled.h1``;
+
+const Text = styled.p`
+    background: #d6ffe0;
+    padding: 10px 0px 10px 0px;
+`;
 
 export default function About() {
     return (
-        <div>
-            <h1>About</h1>
-            <p>About us...</p>
-        </div>
+        <Content>
+            <Title>About</Title>
+            <Text>About us...</Text>
+        </Content>
     );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,23 @@
 import React from "react";
+import styled from "styled-components";
+
+const Content = styled.div`
+    background: #fdb6b6;
+    padding: 10px;
+`;
+
+const Title = styled.h1``;
+
+const Text = styled.p`
+    background: #ffcece;
+    padding: 10px 0px 10px 0px;
+`;
 
 export default function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-            <p>Content.</p>
-        </div>
+        <Content>
+            <Title>Home</Title>
+            <Text>Content.</Text>
+        </Content>
     );
 }
