92a8104ff290c23428d1de183965fa1de0d4d622
Toxe <toxe@toxe.net>
2020-08-05 12:09:25 +0200

    Change to nav tag

--- src/Navigation.js
+++ src/Navigation.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Nav = styled.div`
+const Nav = styled.nav`
     display: flex;
     padding: 10px;
     background: #d3d3ff;
